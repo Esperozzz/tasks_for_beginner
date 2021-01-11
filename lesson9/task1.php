bcf06accb8839e50ef80494bf9a86a803e7ef691
Esperoz <esperoz9@gmail.com>
2021-01-12 00:11:53 +0300

    lesson 9 task 1 - 2 completed

--- lesson9/task1.php
+++ lesson9/task1.php
@@ -1,3 +1,13 @@
 <?php
 
 include_once '../header.php';
+
+$lowerBound = 1;
+$upperBound = 20;
+
+$numbersSet = range($lowerBound, $upperBound);
+$sumOfNumbers = array_sum($numbersSet);
+$numbersCount = count($numbersSet);
+$arithmeticMean = $sumOfNumbers / $numbersCount;
+
+echo 'Arithmetic mean: ' . $arithmeticMean;
--- lesson9/task2.php
+++ lesson9/task2.php
@@ -1,3 +1,11 @@
 <?php
 
 include_once '../header.php';
+
+$lowerBound = 1;
+$upperBound = 100;
+
+$numbersSet = range($lowerBound, $upperBound);
+$sumOfNumbers = array_sum($numbersSet);
+
+echo 'Sum of numbers: ' . $sumOfNumbers;
