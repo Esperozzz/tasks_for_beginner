8987a4c15a4f9f75f896bd54bce2c33914c96d3e
esperozzz <esperoz9@gmail.com>
2021-01-14 22:23:06 +0300

    lesson 11 task 11 completed

--- lesson11/task11.php
+++ lesson11/task11.php
@@ -1,3 +1,9 @@
 <?php
 
 include_once '../header.php';
+
+$monthFormat = 't';
+$currentTimestamp = time();
+$numberOfDaysPerMonth = date($monthFormat, $currentTimestamp);
+
+echo $numberOfDaysPerMonth;
--- lesson11/task12.php
+++ lesson11/task12.php
@@ -1,3 +1,38 @@
 <?php
 
 include_once '../header.php';
+
+define('FIRST_YEAR_OF_OUR_ERA', 1);
+
+$inputSent = 'ok';
+$script = $_SERVER['SCRIPT_NAME'];
+
+print_r($_POST);
+
+if (isset($_POST['submit']) && $_POST['submit'] === $inputSent) {
+    $enteredYear = $_POST['year'];
+    if (
+        $enteredYear > FIRST_YEAR_OF_OUR_ERA
+        || $enteredYear < PHP_INT_MAX
+    ) {
+
+    }
+}
+
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <form action="<?=$script?>" method="post">
+      <label for="year">Enter year</label>
+      <input type="text"  id="year" name="year">
+      <button type="submit" name="submit" value="ok">Confirm</button>
+    </form>
+  </body>
+</html>
+
+ 
