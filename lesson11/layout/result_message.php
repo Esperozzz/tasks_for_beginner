1c038949b11acbf1034511999e3c3eef009246d5
esperozzz <esperoz9@gmail.com>
2021-01-20 00:27:30 +0300

    update lesson 11 task 12

--- lesson11/layout/result_message.php
+++ lesson11/layout/result_message.php
@@ -1,1 +1,8 @@
-<h1 style="color: #088000">Результат: <?=$result?></h1>
+<h2 style="color: #088000">Результат:</h2>
+<ul style="color: #088000">
+<?php foreach($messages as $message): ?>
+    <li><?=$message?></li>
+<?php endforeach; ?>
+</ul>
+
+<?=date('L',$enteredYearTimestamp)?>
