a76158993669c66f4cdf7744eb9bde554c3dfbb8
esperozzz <esperoz9@gmail.com>
2021-01-22 23:38:18 +0300

    lesson 18 task 1 - 2 completed

--- lesson18/task2.php
+++ lesson18/task2.php
@@ -1,3 +1,21 @@
 <?php
 
 include_once '../header.php';
+
+/**
+ * Пример: 1 яблоко, 3 яблока, 8 яблок
+ */
+function pluralNoun(int $count, $one, $twoOfFour, $fiveOrMore): string
+{
+    if ($count > 14 || $count < -14) {
+        $count = $count % 10;
+    }
+    
+    if ($count === 1 || $count === -1) {
+        return $one;
+    } else if (($count >= 2 && $count <= 4) || ($count <= -2 && $count >= -4)) {
+        return $twoOfFour;
+    } else {
+        return $fiveOrMore;
+    }
+}
--- lesson18/task3.php
+++ lesson18/task3.php
@@ -1,3 +1,12 @@
 <?php
 
 include_once '../header.php';
+
+function isHappyTicket(int $num): bool
+{
+    $partOfTheNumber = str_split($num, 3);
+    $sumLefPart = array
+    return $partOfTheNumber[0] === $partOfTheNumber[1];
+}
+
+var_dump(isHappyTicket(654456));
