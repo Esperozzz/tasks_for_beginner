bf1257550cc74989e57674cb98857fa89aa08044
esperozzz <esperoz9@gmail.com>
2021-01-24 13:20:13 +0300

    create task 4

--- lesson18/task4.php
+++ lesson18/task4.php
@@ -1,3 +1,23 @@
 <?php
 
 include_once '../header.php';
+
+function amicableNumbers(int $num1, int $num2): array
+{
+    return [];
+}
+
+function dividersOfNumber(int $num): array
+{
+    $dividersOfNumber = [];
+    
+    if ($num !== 0) {
+        for ($divider = 1; $divider <= $num; $divider++) {
+            if (($num % $divider) === 0) {
+                $dividersOfNumber[] = $divider;
+            }
+        }
+    }
+    
+    return $dividersOfNumber;
+}
